db72eaf9f61ec36916d4fcf4a40ec9d3bc8af59d
jaynagar21 <95155413+jaynagar21@users.noreply.github.com>
2022-01-12 17:50:57 +1300

    5 day forecast - basic/foundation JS

--- src/app.js
+++ src/app.js
@@ -21,6 +21,35 @@ function formatDate(timestamp) {
   return `${day}, ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thurs", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = '<div class="row">';
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+            <div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+                    <img
+                        src="http://openweathermap.org/img/wn/50d@2x.png"
+                        alt=""
+                        width="42"
+                    />
+                <div class="weather-forecast-temperatures">
+                    <span class="weather-forecast-temperature-max"> 18° </span>
+                    <span class="weather-forecast-temperature-min"> 12° </span>
+                </div>
+            </div>
+        `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -29,6 +58,8 @@ function displayTemperature(response) {
   let speedElement = document.querySelector("#speed");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
+
+  displayForecast();
 
   celsiusTemperature = response.data.main.temp;
 
